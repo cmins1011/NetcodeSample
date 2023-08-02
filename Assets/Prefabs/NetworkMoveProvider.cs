dd2a8b4dcefc562d3cf945e26932a3cc937280ba
cmins1011 <chlalstj1005@gmail.com>
2023-08-02 16:43:35 +0900

    simulator test

--- Assets/Prefabs/NetworkMoveProvider.cs
+++ Assets/Prefabs/NetworkMoveProvider.cs
@@ -6,7 +6,9 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class NetworkMoveProvider : ActionBasedContinuousMoveProvider
 {
     [SerializeField]
-    private bool enableInputActions;
+    //private bool enableInputActions;
+    public bool enableInputActions;
+
 
     protected override Vector2 ReadInput(){
         if (!enableInputActions) return Vector2.zero;
